374a5b719d720e8441192ed468d254fcaf93ce47
Sean <seanchowdhury@gmail.com>
2026-04-04 12:13:43 -0400

    removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { MapProvider } from "react-map-gl/maplibre";
 import "./App.css";
 import MapView from "./pages/MapView/MapView";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <MapProvider>
       <MapView />
-      <Analytics />
     </MapProvider>
   );
 }
