d8669b5d77e95c78484cf7f55e4412e77078e9e1
Sean <seanchowdhury@gmail.com>
2026-04-04 12:31:56 -0400

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MapProvider } from "react-map-gl/maplibre";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import MapView from "./pages/MapView/MapView";
 
 export default function App() {
   return (
     <MapProvider>
+      <Analytics />
       <MapView />
     </MapProvider>
   );
